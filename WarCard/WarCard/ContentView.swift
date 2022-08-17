0cc086f5c5123a6171baead4c79b46f1c9d5c853
Veer Singh <veersingh209@icloud.com>
2022-08-17 16:48:44 -0700

    init commit

---
+++ WarCard/WarCard/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WarCard
+//
+//  Created by Veer Singh on 8/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WarCard/WarCard/WarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardApp.swift
+//  WarCard
+//
+//  Created by Veer Singh on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
