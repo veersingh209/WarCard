f215fcbc6342e1a9523ce63cc418324b328f3b21
Veer Singh <veersingh209@icloud.com>
2022-08-17 17:00:42 -0700

    Main UI

--- WarCard/WarCard/ContentView.swift
+++ WarCard/WarCard/ContentView.swift
@@ -9,7 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        ZStack {
+            Image("background")
+                .ignoresSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                .padding()
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                .padding()
+                Spacer()
+                
+            }
+            .padding()
+            
+        }
+        
     }
 }
 
